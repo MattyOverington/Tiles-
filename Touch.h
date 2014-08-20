04c8dcb3a85c8be0efa4850172f8bdeae1c77b90
Matty Overington <1023604@student.ccgs.wa.edu.au>
2014-08-20 12:04:04 +0800

    qwrwer
    sdfsfd

---
+++ Touch.h
@@ -0,0 +1,16 @@
+//
+//  Touch.h
+//  Tiles!® Board Controller
+//
+//  Created by Matthew Overington on 8/18/14.
+//  Copyright (c) 2014 Matthew Overington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Touch : NSObject
+
+@property NSInteger x;
+@property NSInteger y;
+
+@end
